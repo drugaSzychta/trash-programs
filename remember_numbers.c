6c3b0e514904378d8ee53ffd90f8d545ca4cae04
drugaSzychta <94285429+drugaSzychta@users.noreply.github.com>
2021-12-05 09:59:57 +0100

    Update remember_numbers.c

--- remember_numbers.c
+++ remember_numbers.c
@@ -1,9 +1,10 @@
 /******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
+The aim of a program is to exercise memorizing digits. The user is asked to type how many
+digits he/she wants to memorize(user_type_len), then computer generate a array.
+Result is stored in (gen_num_arr). The next step is to print random numbers to the screen.
+Then, the string from an input is stored in user_ch_arr. Check() function converts char to int and compares
+two arrays. Information about check is stored in the bool flag.
 
 *******************************************************************************/
 #include <stdio.h>
@@ -11,12 +12,14 @@ Code, Compile, Run and Debug online from anywhere in world.
 #include <time.h>
 #include <stdbool.h>
 
-#define N 10
-
-int* gen_num_arr;
-char user_arr[N];
-bool flag = true;
+//variables initialized in generate_num()
 int user_type_len;
+int* gen_num_arr;
+//array initialized in user_input()
+char* user_ch_arr;
+size_t buff_size=100;
+//variables used in check() and print_result()
+bool flag = true;
 
 void generate_num();
 void print_gen_numbers(int *arr);
@@ -28,16 +31,16 @@ int main()
 {
     generate_num();
     print_gen_numbers(gen_num_arr);
-    /*user_input();
+    user_input();
     check();
-    print_result();*/
+    print_result();
     
     return 0;
 }
 
 void generate_num(){
     srand(time(NULL));
-    printf("How many numbers do you want to remember?");
+    printf("How many numbers do you want to remember?Must be less than 100: ");
     scanf("%d",&user_type_len);
     while(getchar()!='\n');
     gen_num_arr=(int*) malloc(sizeof(int)*user_type_len);
@@ -45,21 +48,34 @@ void generate_num(){
 }
 void print_gen_numbers(int *arr){
     char x;
-    for(int *p=arr;p<arr+N;p++) printf("%d",*p);
+    for(int *p=arr;p<arr+user_type_len;p++) printf("%d",*p);
     printf("\n");
     printf("Are you ready? Press y to start: \n");
     scanf("%c",&x);
     if(getchar()!='\n');
     if(x!='y') exit(1);
-    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    //printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 }
 void user_input(){
+    /********************************************************
+     * We use dynamic allocation because a length of a user input isn't constant.
+     * Instead getline()(which is is a standard library from 2010) we could use a fgets
+     * size_t is required to getline
+     * for loop is my personal debugger stdbool
+     * ******************************************************/
+    user_ch_arr =(char*) malloc(sizeof(char)*(user_type_len+1));
     printf("Type numbers:\n");
-    scanf("%4[^n]",user_arr);
+    size_t characters = getline(&user_ch_arr, &buff_size,stdin);
+    printf("Number of characters: %zu \n",characters);
+    for(int i=0;i<user_type_len;i++) printf("%c ",user_ch_arr[i]);
 }
 void check(){
-    for(int i=0;i<N;i++){
-        int user_element = (int)user_arr[i]-48;
+    /*******************************
+     * Algorithm is very simple. We convert char ASCII numbers to int numbers, that's why
+     * every char is substracted by 48.
+     * *****************************/
+    for(int i=0;i<user_type_len;i++){
+        int user_element = (int)user_ch_arr[i]-48;
         if(user_element!=gen_num_arr[i]) flag=false;
     }
 }
