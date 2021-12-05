ddff67c800f2e390a96828d8290366a8448e69bc
drugaSzychta <94285429+drugaSzychta@users.noreply.github.com>
2021-12-05 08:48:08 +0100

    Update remember_numbers.c

--- remember_numbers.c
+++ remember_numbers.c
@@ -11,11 +11,12 @@ Code, Compile, Run and Debug online from anywhere in world.
 #include <time.h>
 #include <stdbool.h>
 
-#define N 4
+#define N 10
 
-int gen_num_arr[N];
+int* gen_num_arr;
 char user_arr[N];
 bool flag = true;
+int user_type_len;
 
 void generate_num();
 void print_gen_numbers(int *arr);
@@ -27,16 +28,20 @@ int main()
 {
     generate_num();
     print_gen_numbers(gen_num_arr);
-    user_input();
+    /*user_input();
     check();
-    print_result();
+    print_result();*/
     
     return 0;
 }
 
 void generate_num(){
     srand(time(NULL));
-    for(int i=0;i<N;i++) gen_num_arr[i]= rand()% 10;
+    printf("How many numbers do you want to remember?");
+    scanf("%d",&user_type_len);
+    while(getchar()!='\n');
+    gen_num_arr=(int*) malloc(sizeof(int)*user_type_len);
+    for(int* p=gen_num_arr;p<gen_num_arr+user_type_len;p++) *p = rand()% 10;
 }
 void print_gen_numbers(int *arr){
     char x;
@@ -55,10 +60,10 @@ void user_input(){
 void check(){
     for(int i=0;i<N;i++){
         int user_element = (int)user_arr[i]-48;
-        //printf("User number after conversion: %d ",user_element);
         if(user_element!=gen_num_arr[i]) flag=false;
     }
 }
 void print_result(){
-    flag==true ? printf("Bezbłędnie") : printf("Błąd");
+    flag==true ? printf("You are a winner") : printf("Sorry...");
 }
+
